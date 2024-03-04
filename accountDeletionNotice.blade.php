8b236a1bf58b64e86350ec70a6aff207f266d1b6
Virus Gaming <93107921+Virus-Gaming@users.noreply.github.com>
2024-03-04 14:00:08 -0800

    Create accountDeletionNotice.blade.php

---
+++ accountDeletionNotice.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account Deletion Notice</title>
+</head>
+<body>
+    <h1>Account Deletion Notice</h1>
+    <p>Hello {{ $username }},</p>
+    <p>We noticed that your account username is numeric, which violates our policies. Therefore, we regret to inform you that your account will be deleted. If you believe this is a mistake, please contact our support team immediately.</p>
+    <p>Thank you for your understanding.</p>
+    <p>Best regards,<br>The Team</p>
+</body>
+</html>
