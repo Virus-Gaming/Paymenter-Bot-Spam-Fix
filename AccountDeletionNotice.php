de7a61ee8632ef251fec9f27628bc3d6e6f22fd9
Virus Gaming <93107921+Virus-Gaming@users.noreply.github.com>
2024-03-04 13:58:26 -0800

    Create AccountDeletionNotice.php
    Added File

---
+++ AccountDeletionNotice.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AccountDeletionNotice extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Account Deletion Notice')
+                    ->view('emails.accountDeletionNotice')
+                    ->with([
+                        'username' => $this->user->username,
+                    ]);
+    }
+}
